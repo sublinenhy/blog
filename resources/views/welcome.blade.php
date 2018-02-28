5f06f1a0afcafbb8b29dded1ef39a27f28de0a54
camden <3293676794@qq.com>
2018-02-28 14:20:23 +0800

    修改主页title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to My First Laravel blog
+                    This is camden
                 </div>
 
                 <div class="links">
